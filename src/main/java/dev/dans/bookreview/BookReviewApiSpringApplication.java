32178a418fcd96ce12ced11b1ce78c7184c1928a
douglasdans <douglas.dans@gmail.com>
2025-03-05 21:30:36 -0300

    feat: adiciona arquivos iniciais

---
+++ src/main/java/dev/dans/bookreview/BookReviewApiSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.dans.bookreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReviewApiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookReviewApiSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/dans/bookreview/BookReviewApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dans.bookreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewApiSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
