50dbb621ae3588978c5358a9ba34961576a0b7ee
douglasdans <douglas.dans@gmail.com>
2025-03-14 10:58:08 -0300

    feat: adiciona remoção de avaliações ao deletar livros e usuários

--- src/main/java/dev/dans/bookreview/application/usecase/book/DeleteBookUseCase.java
+++ src/main/java/dev/dans/bookreview/application/usecase/book/DeleteBookUseCase.java
@@ -1,18 +1,22 @@
 package dev.dans.bookreview.application.usecase.book;
 
 import dev.dans.bookreview.domain.repository.BookRepository;
+import dev.dans.bookreview.domain.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DeleteBookUseCase {
     private final BookRepository bookRepository;
+    private final ReviewRepository reviewRepository;
 
-    public DeleteBookUseCase(BookRepository bookRepository) {
+    public DeleteBookUseCase(BookRepository bookRepository, ReviewRepository reviewRepository) {
         this.bookRepository = bookRepository;
+        this.reviewRepository = reviewRepository;
     }
 
     public void execute(Long id) {
+        reviewRepository.deleteAllByBookId(id);
         bookRepository.deleteById(id);
     }
 }
--- src/main/java/dev/dans/bookreview/application/usecase/user/DeleteUserUseCase.java
+++ src/main/java/dev/dans/bookreview/application/usecase/user/DeleteUserUseCase.java
@@ -1,19 +1,24 @@
 package dev.dans.bookreview.application.usecase.user;
 
+import dev.dans.bookreview.domain.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import dev.dans.bookreview.domain.repository.UserRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class DeleteUserUseCase {
     private final UserRepository userRepository;
+    private final ReviewRepository reviewRepository;
 
-    public DeleteUserUseCase(UserRepository userRepository) {
+    public DeleteUserUseCase(UserRepository userRepository, ReviewRepository reviewRepository) {
         this.userRepository = userRepository;
+        this.reviewRepository = reviewRepository;
     }
 
     public void execute(Long id) {
+        reviewRepository.deleteAllByUserId(id);
         userRepository.deleteById(id);
     }
 }
