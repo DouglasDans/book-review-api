1cca18b1f9ea19d3df8980850228cfd548a11a97
douglasdans <douglas.dans@gmail.com>
2025-03-14 11:23:30 -0300

    feat: adiciona tratamento de exceções para métodos não permitidos, violações de integridade de dados e restrições de banco de dados

--- src/main/java/dev/dans/bookreview/infra/handlers/GlobalExceptionHandler.java
+++ src/main/java/dev/dans/bookreview/infra/handlers/GlobalExceptionHandler.java
@@ -5,12 +5,16 @@ import dev.dans.bookreview.infra.response.RestResponseBuilder;
 import dev.dans.bookreview.shared.exceptions.ResourceDataNullException;
 import dev.dans.bookreview.shared.exceptions.ResourceNotFoundException;
 import dev.dans.bookreview.shared.utils.GetResponseSelfLink;
+import jakarta.validation.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -34,6 +38,46 @@ public class GlobalExceptionHandler {
                 GetResponseSelfLink.getSelfLink(),
                 false,
                 HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<RestResponse<String>> resourceDataNull(HttpRequestMethodNotSupportedException ex) {
+        return RestResponseBuilder.build(
+                "Method Not Allowed",
+                GetResponseSelfLink.getSelfLink(),
+                false,
+                HttpStatus.METHOD_NOT_ALLOWED
+        );
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<RestResponse<String>> resourceDataNull(DataIntegrityViolationException ex) {
+        return RestResponseBuilder.build(
+                "Data Integrity Violation",
+                GetResponseSelfLink.getSelfLink(),
+                false,
+                HttpStatus.CONFLICT
+        );
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<RestResponse<String>> resourceDataNull(ConstraintViolationException ex) {
+        return RestResponseBuilder.build(
+                "Database Constraint Violation",
+                GetResponseSelfLink.getSelfLink(),
+                false,
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<RestResponse<String>> resourceDataNull(NoResourceFoundException ex) {
+        return RestResponseBuilder.build(
+                ex.getMessage(),
+                GetResponseSelfLink.getSelfLink(),
+                false,
+                HttpStatus.NOT_FOUND
         );
     }
 
