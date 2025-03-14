1db6828b67b168a61d27f753c9e85ba5f94de57c
douglasdans <douglas.dans@gmail.com>
2025-03-14 07:23:45 -0300

    feat: adiciona hash de senha ao criar usuário

--- src/main/java/dev/dans/bookreview/application/usecase/user/CreateUserUseCase.java
+++ src/main/java/dev/dans/bookreview/application/usecase/user/CreateUserUseCase.java
@@ -4,18 +4,22 @@ import dev.dans.bookreview.domain.entities.User;
 import dev.dans.bookreview.domain.repository.UserRepository;
 import dev.dans.bookreview.shared.exceptions.ResourceDataNullException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CreateUserUseCase {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public CreateUserUseCase(UserRepository userRepository) {
+    public CreateUserUseCase(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User execute(User user){
         verifyNullAttributes(user);
+        user.setPassword(hashPassword(user.getPassword()));
         return userRepository.save(user);
     }
 
@@ -26,5 +30,9 @@ public class CreateUserUseCase {
         if (user.getPassword() == null || user.getPassword().isEmpty()) {
             throw new ResourceDataNullException("User password");
         }
+    }
+
+    private String hashPassword(String password) {
+        return passwordEncoder.encode(password);
     }
 }
